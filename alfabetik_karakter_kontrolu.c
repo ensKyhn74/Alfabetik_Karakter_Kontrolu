e68d7b1b52dfce18e633167d815641c951f86221
ensKyhn74 <117343032+ensKyhn74@users.noreply.github.com>
2022-11-06 11:09:47 +0300

    Add files via upload

---
+++ alfabetik_karakter_kontrolu.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// klavyeden girilen karakter alfabetik mi değil mi?
+
+int main() 
+
+{
+ 
+ char ch;
+  
+ printf("Bir karakter girin: ");
+ scanf("%c",&ch);
+ 
+ if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) 
+    
+  printf("%c alfabe karakteri.",ch);
+     
+ else
+    
+  printf("%c alfabe karakteri degil.",ch);
+
+ 
+  return 0;
+
+}
